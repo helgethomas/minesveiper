e4472db7d41f02fbb2c27605bba8c7d18af2c08c
Helge Thomas Hellerud <helge.thomas.hellerud@nrk.no>
2015-11-17 13:36:47 +0100

    Initial commit

---
+++ test/game-tests.js
@@ -0,0 +1,8 @@
+var game = require("../src/game");
+var assert = require("chai").assert;
+
+describe("Test case", function () {
+	it("does something useful", function () {
+		console.log(game.hei);
+	});
+});
